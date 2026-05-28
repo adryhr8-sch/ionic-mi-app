ff2798c267123e83462721abf3ab0cee15b6cc3d
adryhr8-sch <adrian.hernandezramirez@cesunbc.edu.mx>
2026-05-28 14:41:54 -0700

    buildapp

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.miapp.app',
+  appName: 'miapp',
+  webDir: 'www'
+};
+
+export default config;
