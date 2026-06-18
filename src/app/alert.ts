9f5e930dcbba39c0c6827e1d35f98545ba9f3c11
adryhr8-sch <adrian.hernandezramirez@cesunbc.edu.mx>
2026-06-17 22:51:12 -0700

    alert

---
+++ src/app/alert.ts
@@ -0,0 +1,47 @@
+import { inject, Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular/standalone';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Alert {
+  private alertController: AlertController = inject(AlertController);
+
+  async showAlert(headerText: string, messageText: string) {
+    const alert = await this.alertController.create({
+      header: headerText,
+      message: messageText,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async confirmAlert(
+    header: string,
+    message: string,
+    confirmText: string = 'Aceptar',
+    cancelText: string = 'Cancelar',
+    funcionOk: Function,
+  ) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: [
+        {
+          text: cancelText,
+          role: 'cancel',
+        },
+        {
+          text: confirmText,
+          role: 'confirm',
+          handler: () => {
+            funcionOk();
+          }
+        }
+      ],
+    });
+
+    await alert.present();
+  }
+}
